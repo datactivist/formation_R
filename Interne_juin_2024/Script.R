2d878f7446dab4a7d1548eb0c489c45c2b93abd9
DianeThy <diane.thierry.3asd@gmail.com>
2024-06-07 17:00:06 +0200

    rename()

--- Interne_juin_2024/Script.R
+++ Interne_juin_2024/Script.R
@@ -9,3 +9,9 @@ library(readxl) #install.packages("readxl") à éxecuter dans la console si pas 
 data <- read_delim("https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/demographyref-france-prenoms-departement-millesime/exports/csv?lang=fr&refine=birth_year%3A%221900%22&facet=facet(name%3D%22birth_year%22%2C%20disjunctive%3Dtrue)&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B", delim = ";")
     # en excel via l'import du fichier sur l'ordinateur
 data <- read_excel("~/Downloads/demographyref-france-prenoms-departement-millesime.xlsx")
+
+# Renommage
+data <- data |> rename(annee_naissance = `Année de naissance`,
+                       code_departement = `Code Officiel Département`,
+                   	   nom_departement = `Nom Officiel Département`,
+                   	   nb_naissances = `Nombre de naissances`)
