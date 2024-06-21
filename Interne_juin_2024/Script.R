43260295d1533e472ba1312c2e593d2b147d2086
DianeThy <diane.thierry.3asd@gmail.com>
2024-06-21 16:38:54 +0200

    séance n°3

--- Interne_juin_2024/Script.R
+++ Interne_juin_2024/Script.R
@@ -8,10 +8,16 @@ library(readxl) #install.packages("readxl") à éxecuter dans la console si pas 
     # en CSV via le lien
 data <- read_delim("https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/demographyref-france-prenoms-departement-millesime/exports/csv?lang=fr&refine=birth_year%3A%221900%22&facet=facet(name%3D%22birth_year%22%2C%20disjunctive%3Dtrue)&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B", delim = ";")
     # en excel via l'import du fichier sur l'ordinateur
-data <- read_excel("~/Downloads/demographyref-france-prenoms-departement-millesime.xlsx")
+#data <- read_excel("~/Downloads/demographyref-france-prenoms-departement-millesime.xlsx")
 
 # Renommage
 data <- data |> rename(annee_naissance = `Année de naissance`,
                        code_departement = `Code Officiel Département`,
                    	   nom_departement = `Nom Officiel Département`,
                    	   nb_naissances = `Nombre de naissances`)
+
+# Sélection
+dat_select1 <- data |> select(-c(annee_naissance, `Année triable`))
+dat_select2 <- data |> select(Sexe:nb_naissances)
+dat_relocate1 <- data |> relocate(code_departement)
+dat_select3 <- data |> select(code_departement, 1:3, 5:9)
