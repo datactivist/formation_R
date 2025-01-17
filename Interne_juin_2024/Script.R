182a21f76065777219db27500d4d54c727912e60
DianeThy <diane.thierry.3asd@gmail.com>
2025-01-17 15:14:36 +0100

    left_join()

--- Interne_juin_2024/Script.R
+++ Interne_juin_2024/Script.R
@@ -1,8 +1,8 @@
 #--- Introduction à R, script contenant les commandes
 
 # Chargement des librairies
-library(tidyverse) #install.packages("tidyverse") à éxecuter dans la console si pas déjà installé
-library(readxl) #install.packages("readxl") à éxecuter dans la console si pas déjà installé
+library(tidyverse) #install.packages("tidyverse") à exécuter dans la console si pas déjà installé
+library(readxl) #install.packages("readxl") à exécuter dans la console si pas déjà installé
 
 # Import des données
     # en CSV via le lien
@@ -10,13 +10,13 @@ data <- read_delim("https://public.opendatasoft.com/api/explore/v2.1/catalog/dat
     # en excel via l'import du fichier sur l'ordinateur
 #data <- read_excel("~/Downloads/demographyref-france-prenoms-departement-millesime.xlsx")
 
-# Renommage
+# Renommer
 data <- data |> rename(annee_naissance = `Année de naissance`,
                        code_departement = `Code Officiel Département`,
                    	   nom_departement = `Nom Officiel Département`,
                    	   nb_naissances = `Nombre de naissances`)
 
-# Sélection
+# Sélectionner
 dat_select1 <- data |> select(-c(annee_naissance, `Année triable`))
 dat_select2 <- data |> select(Sexe:nb_naissances)
 dat_relocate1 <- data |> relocate(code_departement)
@@ -32,7 +32,7 @@ data2 <- data |> mutate(nb_naissances10 = nb_naissances * 10,
                         taux = nb_naissances / sum(nb_naissances) *100,
                         siecle = substr(`Année triable`, 1, 2),
                         siecle = as.numeric(siecle) + 1)
-    #exos en plus créer
+    #exos en plus "créer"
 data <- data |> mutate(region_1 = word(`Nom Officiel Région`, 1))
 data <- data |> mutate(new_col = str_replace_all(Sexe, c("Masculin" = "Hommme", "Féminin" ~ "Femme")))
 data <- data |> mutate(nouvelle_colonne = case_when(Sexe == "Masculin" ~ "Hommme",
@@ -43,3 +43,17 @@ data <- data |> mutate(nouvelle_colonne = case_when(Sexe == "Masculin" ~ "Hommme
 data |> summarise(moy = mean(nb_naissances),
                   med = median(nb_naissances))
 data |> summarise(etendue = range(nb_naissances))
+
+# Grouper
+data |> summarise(naissances_sexe = sum(nb_naissances), .by = Sexe)
+data |> mutate(n_reg = n(), .by = `Nom Officiel Région`)
+data |> filter(row_number() == 1, .by = `Nom Officiel Région`)
+
+# Joindre
+pop_departements <- read_delim("https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/demographyref-france-pop-active-sexe-activite-departement-millesime/exports/csv?lang=fr&refine=year%3A%222019%22&facet=facet(name%3D%22year%22%2C%20disjunctive%3Dtrue)&timezone=Europe%2FBerlin&use_labels=true&delimiter=%3B", ";") |> 
+    filter(`Nom de la variable` == "Femmes Actifs ayant un emploi") |> 
+    select(`Code Officiel Département`, Valeur) |> 
+    rename(pop_femmes_actives = Valeur)
+jointure <- data |> 
+    left_join(pop_departements, by = c("code_departement" = "Code Officiel Département"))
+
